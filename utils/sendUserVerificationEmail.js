72f6f0d8ccd3db02add59b3fd2e524a1ba7142f6
mtskei <michel.tcha@gmail.com>
2022-03-14 10:41:20 +0100

    init

--- utils/sendUserVerificationEmail.js
+++ utils/sendUserVerificationEmail.js
@@ -1,7 +1,7 @@
 const sendEmail = require("./sendEmail");
 
 const sendUserVerificationEmail = async ({ email, verificationToken }) => {
-	const domain = process.env.DOMAIN || "https://pfeedback.micheltcha.com";
+	const domain = process.env.DOMAIN || "https://fm.pfeedback.micheltcha.com";
 
 	const emailTitle = "Verify your account";
 	const html = `<p>Please click the following link to activate your account: <a href="${domain}/new-account/verify-email?token=${verificationToken}&email=${email}">Confirm email</a> </p>`;
